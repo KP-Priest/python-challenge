85b810d4a1d1b554301de45c2275d4324cde0ff3
kp-priest <kp@heykp.ninja>
2024-10-17 21:48:49 -0500

    Updated files path after relocating files

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,8 +6,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("analysis", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
+file_to_load = os.path.join("/Users/kp/Git/python-challenge/PyBank/Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("/Users/kp/Git/python-challenge/PyBank/analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
 total_months = 0
