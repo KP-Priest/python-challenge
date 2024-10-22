5079aba597668e53a58ecac80f12c8f5e490bf05
kp-priest <kp@heykp.ninja>
2024-10-21 21:27:15 -0500

    Saving progress

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,11 +6,12 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("users/KP/Git/python-Challenge/PyPoll/Resources", "election_data.csv")  # Input file path
-file_to_output = os.path.join("users/KP/Git/python-challenge/PyPoll/analysis", "election_analysis.txt")  # Output file path
+file_to_load = os.path.join("/users/kp/Git/python-challenge/PyPoll/Resources", "election_data.csv")  # Input file path
+file_to_output = os.path.join("/users/kp/Git/python-challenge/PyPoll/analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables to track the election data
 total_votes = 0  # Track the total number of votes cast
+candidate = ""   # holds candidate name
 vote_getters = [] # list of candidates who received votes
 
 # Define lists and dictionaries to track candidate names and vote counts
@@ -19,7 +20,7 @@ candidate_vote_count = {} # 'candidate': vote count
 # Winning Candidate and Winning Count Tracker
 election_winner = ""
 winning_vote_count = 0
-windding_percentage = 0
+winning_percentage = 0
 
 # Open the CSV file and process it
 with open(file_to_load) as election_data:
@@ -31,27 +32,31 @@ with open(file_to_load) as election_data:
     # Loop through each row of the dataset and process it
     for row in reader:
 
-
-
-        
-
         # Print a loading indicator (for large datasets)
         print(". ", end="")
 
-        # Increment the total vote count for each row
+        #print(vote_getters) # test code up to this point
 
+
+        # Increment the total vote count for each row
+        total_votes =+ 1
 
         # Get the candidate's name from the row
-
-
         # If the candidate is not already in the candidate list, add them
+        candidate = row[2]
+        if candidate not in vote_getters:
+            vote_getters.append(candidate)
+            candidate_vote_count[candidate] = 0
+
+            # Add a vote to the candidate's count
+            candidate_vote_count[candidate] += 1
+            # print candidate_vote_count to test code
+            print(candidate_vote_count)
 
 
-        # Add a vote to the candidate's count
-
-
-# Open a text file to save the output
-with open(file_to_output, "w") as txt_file:
+    # Open a text file to save the output
+    
+    # with open(file_to_output, "w") as txt_file:
 
     # Print the total vote count (to terminal)
 
@@ -71,7 +76,7 @@ with open(file_to_output, "w") as txt_file:
         # Print and save each candidate's vote count and percentage
 
 
-    # Generate and print the winning candidate summary
+        # Generate and print the winning candidate summary
 
 
-    # Save the winning candidate summary to the text file
+        # Save the winning candidate summary to the text file
